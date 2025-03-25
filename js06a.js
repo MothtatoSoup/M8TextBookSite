03f80138ddafb6c803e556cb73b834ede93a42ff
MothtatoSoup <maddy.puryear@outlook.com>
2025-03-25 13:28:03 -0400

    completed page 216

--- js06a.js
+++ js06a.js
@@ -10,5 +10,28 @@
       Filename: js06a.js
  */
 
+window.addEventListener("load", function(){
+      let orderForm = document.forms.orderForm;
+      let model = orderForm.elements.model;
 
+      // Select Model selection list when form opens
+      model.focus();
+
+      // Calculate the cost o fthe ordr
+      calcOrder();
+
+      function calcOrder(){
+            //Determine the selected model
+            let mIndex = model.selectedIndex;
+            let mValue = model.options[mIndex].value;
+
+            //Determine the selected quantity
+            let qIndex = orderForm.elements.qty.selectedIndex;
+            let quantity = orderForm.elements.qty[qIndex].value;
+
+            //Model cost = model cost * quantity
+            let modelCost = mValue*quantity;
+            orderForm.elements.modelCost.value = modelCost;
+      }
+});
 
